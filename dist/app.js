7a06c54e36a40c0033a059853ff07a6f822ecc64
kadrikondi <abdulkadri42@gmail.com>
2020-01-13 02:10:16 +0100

    graphic vila

--- dist/app.js
+++ dist/app.js
@@ -7,12 +7,27 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 var routes = require('./routes/index');
 
+var path = require('path');
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+//static decl
+app.use(express.static(path.join(__dirname, 'client/build')));
+//production mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', function (req, res) {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    });
+}
 
+//build mode
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
 app.use('/', routes);
 var port = 3001 || process.env.PORT;
 
--- server/app.js
+++ server/app.js
@@ -5,12 +5,21 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes/index')
 
+const path = require('path');
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+//static decl
+app.use(express.static(path.join(__dirname, 'client/build')));
+//production mode
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  
+  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
 
+  //build mode
+  app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 app.use('/', routes)
 const port = 3001 || process.env.PORT
 
