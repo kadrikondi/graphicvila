0a30aa9677cae84a687eef3e515ec71af3652b85
kadrikondi <abdulkadri42@gmail.com>
2020-01-14 01:04:40 +0100

    modifies app

--- server/app.js
+++ server/app.js
@@ -18,8 +18,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  
   app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
 
-  //build mode
-  app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+//   //build mode
+//   app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'../client/public/index.html'));})
 app.use('/', routes)
 const port = 3001 || process.env.PORT
 
