c5a4d20d82149280c832fce20d70b094d63a67e8
otitoju <otitojuoluwapelumi@gmail.com>
2020-06-18 20:25:36 +0100

    change route

--- server/app.js
+++ server/app.js
@@ -19,13 +19,23 @@ app.use(bodyParser.urlencoded({
 app.use('/', routes)
 
 //production mode
-//if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build'))
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next()
+  })
+  
+  
+  //if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     })
-//}
+ // }
 
 
 app.listen(PORT, () => {
